4eb75e6cfd894fecf84f52ad4072b8811f8efad7
Yucen Liu <yucen.u@gmail.com>
2021-06-08 23:21:09 +0000

    Add Delete URls function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+const PORT = 8081; // default port 8080
 app.set("view engine", "ejs"); // tells express to use ejs as the templating engine 
+app.use(bodyParser.urlencoded({extended: true}));
 
+function generateRandomString() {};
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,15 +15,41 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.redirect(`/urls/:shortURL`);         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  // server generates a shortURI, and adds it to the urlDatabase
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");//html art => making "hello" bold 
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  // console.log("URL deleted", URLToBeDeleted)
+  res.redirect('/urls');
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
